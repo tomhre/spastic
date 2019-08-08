fd60ee3e21204aae5b75bce04cba1e6e0982bfbd
Tomas Hreha <tomhrecom@gmail.com>
2019-08-08 09:19:59 +0100

    create+validation, starting update+validation / TODO frontend error handling

--- ttspastic/app/Http/Controllers/API/ClientController.php
+++ ttspastic/app/Http/Controllers/API/ClientController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Clients\Client;
+use App\Http\Requests\CreateClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+
 
 class ClientController extends Controller
 {
@@ -14,8 +17,13 @@ class ClientController extends Controller
         return Client::all();
     }
     
-    public function store(Request $request)
+    public function store(CreateClientRequest $request)
     {
         return Client::create($request->validated());
+    }
+    
+    public function update(UpdateClientRequest $request, Client $client)
+    {
+        dd($client);
     }
 }
